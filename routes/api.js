ff90cd32309f8676909864e65b7a181384286e61
Florent Calabresi <60184656+florentcalabresi@users.noreply.github.com>
2024-06-11 01:20:30 +0200

    Check client status

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const verifyToken = require("../middleware/verifyToken");
 const express = require("express");
 const router = express.Router();
 
-router.post('/test', verifyToken, (req, res) => {
+router.post('/notify', verifyToken, (req, res) => {
     const io = req.app.get('socketio');
     const clients = req.app.get('clients');
     const { user_id, channel, data } = req.body;
@@ -19,6 +19,20 @@ router.post('/test', verifyToken, (req, res) => {
         io.to(`${channel}_${client.socket_id}`).emit('notification', { channel: channel, data: data });
     })
     res.json({ channel: channel, message: 'Message sent' });
+});
+
+router.post('/client/', verifyToken, (req, res) => {
+    const io = req.app.get('socketio');
+    const clients = req.app.get('clients');
+    const { client_id } = req.body
+
+    if (!client_id) {
+        return res.status(400).json({ error: 'Client is required' });
+    }
+
+    const client = clients.find((client) => client.user_id == client_id)
+
+    res.json({ client: client });
 });
 
 module.exports = router;
